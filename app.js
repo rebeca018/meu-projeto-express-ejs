075acd7fc9cb18ad72779e01c7ffa4380a224663
rebeca018 <rebecasnascimento018@gmail.com>
2023-08-31 16:28:35 -0400

    Commit produto

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
   res.render('index', { message: 'Olá, Mundo!' });
 });
 
+app.get('/produtos', (req, res) => {
+  res.render('produtos', { message: 'Deu certo' });
+});
+
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
