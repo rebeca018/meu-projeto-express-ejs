2f8cdfa97db981d709b3699b9d4f7983e5d341c3
rebeca018 <rebecasnascimento018@gmail.com>
2023-11-16 16:44:20 -0400

    Commit começando a listar os produtos na view

--- app.js
+++ app.js
@@ -19,8 +19,13 @@ const produtos = [
 
 ]
 
+function buscarProdutoPorID(id){
+  const produto = produtos.find(produto => produto.id == id);
+  return produto || null
+}
+
 app.get('/', (req, res) => {
-  res.render('index', { message: 'Olá, Mundo!' });
+  res.render("index", { produtos });
 });
 
 app.get('/produto', (req, res) => {
